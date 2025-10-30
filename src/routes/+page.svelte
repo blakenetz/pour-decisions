df4697c06b8c226ee2d920ec0f04743704eadc62
Blake Netzeband <blake.netzeband@gmail.com>
2025-10-30 09:29:32 -0600

    create Reshape button component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
-	import tableSettingImage from '$lib/assets/table-setting.png';
+  import tableSettingImage from '$lib/assets/table-setting.png';
+  import ReshapeButton from '$lib/components/ReshapeButton.svelte';
 </script>
 
-<div class="flex flex-col items-center justify-center min-h-screen">
+<div class="flex flex-col items-center min-h-[100dvh] max-h-[100dvh] px-4">
 	<h1 class="text-6xl mb-8">Pour Decisions</h1>
-	<img src={tableSettingImage} alt="Table Setting" class="max-w-2xl w-full" />
+	<div class="flex-1 flex items-center w-full">
+		<img
+			src={tableSettingImage}
+			alt="Table Setting"
+			class="max-h-full max-w-full w-auto h-auto object-contain mx-auto"
+		/>
+	</div>
+	  <div class="mt-8 mb-4 flex gap-4">
+    <ReshapeButton as="a" href="/login" class="inline-block">Login</ReshapeButton>
+    <ReshapeButton as="a" href="/signup" class="inline-block">Sign up</ReshapeButton>
+  </div>
 </div>
