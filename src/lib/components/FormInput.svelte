41ea2ec743ccc6fa375a3a7fbd1b663f2903a1d6
Blake Netzeband <blake.netzeband@gmail.com>
2026-04-30 21:46:04 -0600

    create form input component

---
+++ src/lib/components/FormInput.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+let {
+	label,
+	id,
+	value = $bindable(''),
+	...rest
+}: {
+	label: string
+	id: string
+	value?: string
+	[key: string]: unknown
+} = $props()
+</script>
+
+<div>
+	<label for={id}>{label}</label>
+	<input {id} bind:value {...rest} />
+</div>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -12,6 +12,7 @@ export {
 export { default as LoopLink } from './components/actions/LoopLink.svelte'
 export { default as OvalButton } from './components/actions/OvalButton.svelte'
 export { default as SquigglyLink } from './components/actions/SquigglyLink.svelte'
+export { default as FormInput } from './components/FormInput.svelte'
 export { default as LoginModal } from './components/LoginModal.svelte'
 export { default as Modal } from './components/Modal.svelte'
 export { default as SignupModal } from './components/SignupModal.svelte'
